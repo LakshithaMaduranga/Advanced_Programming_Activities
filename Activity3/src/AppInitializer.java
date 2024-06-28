5f4ff1da26c71c4ab7166b7d85ba6073de43d01b
LakshithaMaduranga <155385829+LakshithaMaduranga@users.noreply.github.com>
2024-06-28 21:45:17 +0530

    first commit

---
+++ Activity3/src/AppInitializer.java
@@ -0,0 +1,6 @@
+public class AppInitializer {
+    public static void main(String[] args) {
+        WideningCasting obj = new WideningCasting();
+        obj.wideningCasting();
+    }
+}
---
+++ Activity3/src/WideningCasting.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class WideningCasting {
+    public void wideningCasting() {
+        Scanner sc = new Scanner(System.in);
+        double num = sc.nextDouble();
+        System.out.println((int) num);
+    }
+
+}
